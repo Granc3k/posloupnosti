69eacfff991759ebda48a18675706580d1d99e83
Martin Šimon <72451051+Granc3k@users.noreply.github.com>
2022-03-15 08:08:29 +0000

    fully working

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-"s_n = (a_n_2/2)*(a_value_1+a_value_2)"
-
-
-
-
-
 function arit_posl(){
     let a_n_1 = document.getElementById("a_n_count_1").valueAsNumber;
-    let a_n_2 = document.getElementById("a_n_count_2").valueAsNumber;
+    let a_n_n = document.getElementById("a_n_count_2").valueAsNumber;
     let a_value_1 = document.getElementById("a_value_count_1").valueAsNumber;
-    let a_value_2 = document.getElementById("a_value_count_2").valueAsNumber;
+    let a_value_n = document.getElementById("a_value_count_2").valueAsNumber;
     let d_count = document.getElementById("d_count").valueAsNumber;
     let count = document.getElementById("v_count").valueAsNumber;
-    let vypis = " ";
-    let a_count;
+    let vypis = " "
+    let a_count = 0
+    let s_n = 0;
 
     if (d_count == 0){
-        d_count = (a_value_2 - a_value_1)/(a_n_2-a_n_1)
+        d_count = (a_value_n - a_value_1)/(a_n_n-a_n_1);
     }
     if (a_n_1 == 0){
-        a_n_1 = a_n_2-(a_value_2 - a_value_1)/d_count;
+        a_n_1 = 1;
     }
-    if (a_n_1 == 1){
-
-    } else{
+    else if (a_n_1 > 1){
         a_n_1=1;
-        a_value_1=a_value_2 + (a_n_1-a_n_2)*d_count;
+        a_value_1=a_value_n + (a_n_1-a_n_n)*d_count;
     }
-    if (a_n_2 == 0){
-        a_n_2 = a_n_1 + (a_value_2 - a_value_1)/d_count;
+    else {
+        
     }
-    let i =0;
+    if (a_n_n == 0){
+        a_n_n = a_n_1 + (a_value_n - a_value_1)/d_count;
+    }
+    if (a_value_1 == 0){
+        a_value_1 = a_value_n-(a_n_n-a_n_1)*d_count;        
+    }
+    if (a_value_n == 0){
+        a_value_n = a_value_1 + d_count*(a_n_n-a_n_1);
+    }
+    let i = 1;
     while (i <= count){
-        a_count = a_n_1 + d*(i - a_n_1);
+        a_count = a_value_1 + d_count*(i-a_n_1);
         vypis = vypis + a_count + ", ";
         i+=1;
     }
@@ -40,15 +42,15 @@ function arit_posl(){
 
 
 
-
  
 
 
-
+    s_n = (a_n_n/2)*(a_value_1+a_value_n)
     document.getElementById("a1").innerHTML= a_value_1;
     document.getElementById("a_n_1_v").innerHTML= a_n_1;
-    document.getElementById("a2").innerHTML= a_value_2;
-    document.getElementById("a_n_2_v").innerHTML= a_n_2;
+    document.getElementById("a2").innerHTML= a_value_n;
+    document.getElementById("a_n_2_v").innerHTML= a_n_n;
     document.getElementById("diference").innerHTML= d_count;
-    document.getElementById("vypis").innerHTML= vypis ;
+    document.getElementById("vypis_a_ap").innerHTML= vypis;
+    document.getElementById("vypis_sn").innerHTML= s_n;
 }
